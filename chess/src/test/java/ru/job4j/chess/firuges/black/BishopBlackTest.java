16bf1e1b8d33dd65b4656620fbbec0632b93f514
RomanKolyagin <romanderyabin@mail.ru>
2023-10-24 00:01:36 +0800

    3. Каркас шахматной доски [#792 #388598]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.chess.firuges.black;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
@@ -9,16 +8,19 @@ import static org.assertj.core.api.Assertions.*;
 public class BishopBlackTest {
 
     @Test
-    public void testPosition() {
-        BishopBlack bb = new BishopBlack(Cell.A1);
+    public void position() {
+        Figure bb = new BishopBlack(Cell.A1);
         Cell expected = Cell.A1;
         assertThat(bb.position()).isEqualTo(expected);
     }
 
     @Test
-    public void testCopy() {
-        BishopBlack bb = new BishopBlack(Cell.A1);
-        Figure expected = bb.copy(Cell.A5);
-        assertThat(expected.position()).isEqualTo(Cell.A5);
+    public void copy() {
+        Figure bb = new BishopBlack(Cell.A1);
+        Cell expected = Cell.G5;
+        bb = bb.copy(Cell.G5);
+        assertThat(bb.position()).isEqualTo(expected);
     }
+
+
 }
