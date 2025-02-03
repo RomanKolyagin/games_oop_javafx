8dde02a406a9074431ac782c9e9dbdd088be1019
DarkSide <romanderyabin@mail.ru>
2025-02-03 16:09:48 +0800

    3. Каркас шахматной доски [#792 #388598]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,14 +1,13 @@
 package ru.job4j.chess;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
     @Test
@@ -19,5 +18,17 @@ public class LogicTest {
             logic.move(Cell.C1, Cell.H6);
         });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveAndFigureOnWayThenOccupiedCellException() throws OccupiedCellException {
+        Logic logic = new Logic();
+        Figure figure = new BishopBlack(Cell.C1);
+        Figure figureOnWay = new BishopBlack(Cell.F4);
+        logic.add(figure);
+        logic.add(figureOnWay);
+        assertThatThrownBy(() -> logic.move(figure.position(), Cell.G5))
+                .isInstanceOf(OccupiedCellException.class)
+                .hasMessage("The cell is occupied");
     }
 }
